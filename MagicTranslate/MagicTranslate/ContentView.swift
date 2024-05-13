94b9d987794d1a76cbc346d77ad5b53dda9e4bc2
DanielePerrupane <148336607+DanielePerrupane@users.noreply.github.com>
2024-05-13 14:12:55 +0200

    first commit

---
+++ MagicTranslate/MagicTranslate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MagicTranslate
+//
+//  Created by Daniele Perrupane on 13/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MagicTranslate/MagicTranslate/MagicTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicTranslateApp.swift
+//  MagicTranslate
+//
+//  Created by Daniele Perrupane on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MagicTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
