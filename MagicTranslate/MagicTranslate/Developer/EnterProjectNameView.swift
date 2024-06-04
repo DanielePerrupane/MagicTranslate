85f74d98eecefa13935c0bd03b42743657e5500b
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2024-06-04 22:48:33 +0200

    Add and customise the titles of the PathPickerView and EnterProjectNameView

--- MagicTranslate/MagicTranslate/Developer/EnterProjectNameView.swift
+++ MagicTranslate/MagicTranslate/Developer/EnterProjectNameView.swift
@@ -15,21 +15,28 @@ struct EnterProjectNameView: View {
     var body: some View {
         @Bindable var localizationData = localizationData
         
-        VStack {
+        VStack(spacing: 20){
             Text("Enter your project name")
+                .font(.title2)
+                .fontWeight(.semibold)
             
-            TextField("Project name: ", text: $localizationData.projectName)
-                .frame(width: 170)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            NavigationLink(destination: ImportXCStringsView()){
-                Text("Done")
-                    .padding(3.0)
-                    .foregroundColor(.white)
+            VStack {
+                TextField("Project name: ", text: $localizationData.projectName)
+                    .frame(width: 170)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                NavigationLink(destination: ImportXCStringsView()){
+                    Text("Done")
+                        .padding(6.0)
+                        .foregroundColor(.white)
+                        .background(
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.greenAccept)
+                        )
+                }
+                .buttonStyle(PlainButtonStyle())
+                .cornerRadius(3.0)
             }
-            .buttonStyle(PlainButtonStyle())
-            .background(.blue)
-            .cornerRadius(3.0)
         }
         .frame(width: 400, height: 400)
         .padding()
--- MagicTranslate/MagicTranslate/PathPickerView.swift
+++ MagicTranslate/MagicTranslate/PathPickerView.swift
@@ -10,32 +10,38 @@ import SwiftUI
 struct PathPickerView: View {
     var body: some View {
         NavigationStack {
-            VStack{
-                NavigationLink(destination: EnterProjectNameView()) {
-                    Text("Developer")
-                        .padding(.horizontal, 20.0)
-                        .padding(.vertical, 10.0)
-                        .foregroundColor(.white)
-                        .background(
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(Color(red: 0.31, green: 0.31, blue: 0.31))
-                        )
-                }
-                .buttonStyle(PlainButtonStyle())
-                .padding(.horizontal)
+            VStack(spacing: 20){
+                Text("Select your role")
+                    .font(.title2)
+                    .fontWeight(.semibold)
                 
-                NavigationLink(destination: ImportMGTRView()) {
-                    Text("Translator")
-                        .padding(.horizontal, 20.0)
-                        .padding(.vertical, 10.0)
-                        .foregroundColor(.white)
-                        .background(
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(Color(red: 0.31, green: 0.31, blue: 0.31))
-                        )
+                VStack {
+                    NavigationLink(destination: EnterProjectNameView()) {
+                        Text("Developer")
+                            .padding(.horizontal, 20.0)
+                            .padding(.vertical, 10.0)
+                            .foregroundColor(.white)
+                            .background(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .fill(Color(red: 0.31, green: 0.31, blue: 0.31))
+                            )
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .padding(.horizontal)
+                    
+                    NavigationLink(destination: ImportMGTRView()) {
+                        Text("Translator")
+                            .padding(.horizontal, 20.0)
+                            .padding(.vertical, 10.0)
+                            .foregroundColor(.white)
+                            .background(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .fill(Color(red: 0.31, green: 0.31, blue: 0.31))
+                            )
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .padding(.horizontal)
                 }
-                .buttonStyle(PlainButtonStyle())
-                .padding(.horizontal)
             }
         }
         .navigationTitle("Select your role")
